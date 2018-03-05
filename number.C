5ecbbe45f4a5cda2ca034defa396d5d52dd12f12
vinothinimarimuthu <31438799+vinothinimarimuthu@users.noreply.github.com>
2018-03-05 15:45:23 +0530

    prgm
    good

---
+++ number.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio,h>
+
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    cout << "Sum = " << sum;
+    return 0;
+}
